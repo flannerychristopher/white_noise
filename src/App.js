ca3edce8b09f17794ccf47188bf215fe9adae1db
flan the dev <flanthedev@gmail.com>
2017-10-26 12:47:30 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>white noise generator</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
